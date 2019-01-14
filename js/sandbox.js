f3535a770657712f33ec325c073e279e2a3420a9
aefoley <ann@prairieopolis.com>
2019-01-14 07:07:25 -0600

    initial commit

--- js/sandbox.js
+++ js/sandbox.js
@@ -20,7 +20,7 @@
     //the last one is the timeline position parameter - you can leave a gap or start it early
     //.to(btn, 0.3, { y: 70, autoAlpha:7, ease:Power1.easeOut})
 
-    .from(img, 0.3, {x: 100, autoAlpha:0, ease:Power1.easeOut}, 1.5)
+   // .from(img, 0.3, {x: 100, autoAlpha:0, ease:Power1.easeOut}, 1.5)
     //'-=1' - that relative positioning - relative to the one before
     .from(h1, 0.3, {y: -15, autoAlpha:0, ease:Power1.easeOut, delay:0.4}, '-=1')
     //this one will run at the 3 second mark - it's called absolute position
@@ -28,11 +28,15 @@
     //.from(p, 0.3, {y: -15, autoAlpha:0, ease:Power1.easeOut}, 'btn-+.5' );
 
 
-
+$(".box").on("mouseenter", function() {
+  var duration = 1;
+  TweenMax.to(this, duration / 4, {y:-50, ease:Power2.easeOut});
+  TweenMax.to(this, duration / 2, {y:0, ease:Bounce.easeOut, delay:duration / 4});
+});
 
 
   
-
+ $('a[title]').qtip();
 
 
 
