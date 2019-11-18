a6c724be5b7152c2245c75a6aabd27e84957d5bc
Ann Foley <ann@prairieopolis.com>
2019-11-18 13:49:55 -0600

    Menu added. Capitol background

--- assets/js/sandbox.js
+++ assets/js/sandbox.js
@@ -7,91 +7,91 @@
   //basic  ------------------------------------------------------------------------  
   
   // // Init ScrollMagic
-  var controller = new ScrollMagic.Controller();
+  // var controller = new ScrollMagic.Controller();
 
-  // //build a scene
-  var gnomeScene = new ScrollMagic.Scene({
-      triggerElement: '.gnomerow',
-      duration:600, //the scene should last for a scroll of 300px
-      triggerHook: .5 //at .7 of viewport height
+  // // //build a scene
+  // var gnomeScene = new ScrollMagic.Scene({
+  //     triggerElement: '.cover',
+  //     duration:600, //the scene should last for a scroll of 300px
+  //     triggerHook: .5 //at .7 of viewport height
 
 
-  })
+  // })
 
-  .setClassToggle('.gnomes', 'fade-in')
-  .addIndicators({
-    name: 'fade and rise gnomes',
-    colorTrigger: 'gray',
-    colorStart:'gray',
-    colorEnd: 'gray'
-  })
-  .addTo(controller);
+  // .setClassToggle('.covergnomes', 'fade-in')
+  // .addIndicators({
+  //   name: 'fade and rise gnomes',
+  //   colorTrigger: 'gray',
+  //   colorStart:'gray',
+  //   colorEnd: 'gray'
+  // })
+  // .addTo(controller);
 
-  // //pinning  ------------------------------------------------------------------------  
+  // // //pinning  ------------------------------------------------------------------------  
 
-  var pinnerScene = new ScrollMagic.Scene({
-    triggerElement: '.symphony',
-    triggerHook: 0,
-    duration: '30%'
-  })
-  .setPin('.symphony')
-  .addIndicators({
-        name: 'pinner',
-        colorTrigger: 'blue',
-        colorStart:'blue',
-        colorEnd: 'blue'
-    })
-  .addTo(controller);
+  // var pinnerScene = new ScrollMagic.Scene({
+  //   triggerElement: '.symphony',
+  //   triggerHook: 0,
+  //   duration: '30%'
+  // })
+  // .setPin('.symphony')
+  // .addIndicators({
+  //       name: 'pinner',
+  //       colorTrigger: 'salmon',
+  //       colorStart:'salmon',
+  //       colorEnd: 'salmon'
+  //   })
+  // .addTo(controller);
 
 
    //parallax scene ---------------------------------------------------------------------
 
-var parallaxTl = new TimelineMax();
-parallaxTl
-    //.from('content-wrapper', 1, {autoAlpha: 0, ease:Power0.easeNone}, 0.3) 
-    .from('.content-wrapper', 0.3, {autoAlpha: 0, ease:Power0.easeNone}, 0.2)
-    .from('.bcg', 2, {y:'-80%', ease:Power0.easeNone}, 0) 
-    ;
+// var parallaxTl = new TimelineMax();
+// parallaxTl
+//     //.from('content-wrapper', 1, {autoAlpha: 0, ease:Power0.easeNone}, 0.3) 
+//     .from('.content-wrapper', 0.3, {autoAlpha: 0, ease:Power0.easeNone}, 0.2)
+//     .from('.bcg', 2, {y:'-80%', ease:Power0.easeNone}, 0) 
+//     ;
 
-var slideParallaxScene = new ScrollMagic.Scene({
-      triggerElement: '.bcg-parallax',
-      triggerHook: 1,
-      duration: '200%' //1 - over the duration of 100% of the scrolling
+// var slideParallaxScene = new ScrollMagic.Scene({
+//       triggerElement: '.bcg-parallax',
+//       triggerHook: 1,
+//       duration: '200%' //1 - over the duration of 100% of the scrolling
 
 
-   })
+//    })
 
-   .setTween(parallaxTl) //2 - we will play the contents of parallaxTl, which has 2 tweens on it.
-   .addIndicators({
-        name: 'parallax',
-        colorTrigger: 'orange',
-        colorStart:'orange',
-        colorEnd: 'orange'
-    })
-   .addTo(controller);
+//    .setTween(parallaxTl) //2 - we will play the contents of parallaxTl, which has 2 tweens on it.
+//    .addIndicators({
+//         name: 'parallax',
+//         colorTrigger: 'orange',
+//         colorStart:'orange',
+//         colorEnd: 'orange'
+//     })
+//    .addTo(controller);
 
 
 
   //with a loop ------------------------------------------------------------------------
 
-$('.looprow').each(function(){
+// $('.looprow').each(function(){
 
-  var balloonScene = new ScrollMagic.Scene({
-    triggerElement: this.children[0],
-    duration:200,
-    triggerHook: .5
-  })
+//   var balloonScene = new ScrollMagic.Scene({
+//     triggerElement: this.children[0],
+//     duration:200,
+//     triggerHook: .5
+//   })
 
-    .setClassToggle(this, 'scoot')
-    .addIndicators({
-        name: 'scooter',
-        colorTrigger: 'green',
-        colorStart:'green',
-        colorEnd: 'green'
-    })
-    .addTo(controller);
+//     .setClassToggle(this, 'scoot')
+//     .addIndicators({
+//         name: 'scooter',
+//         colorTrigger: 'green',
+//         colorStart:'green',
+//         colorEnd: 'green'
+//     })
+//     .addTo(controller);
 
-}) //ends looprow function
+// }) //ends looprow function
 
   
   	// var img = $('img');
